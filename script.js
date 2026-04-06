6df760579535e3e8b2882cd48cafcd58bc9acdd4
klaudia <orczykowskaklaudia@gmail.com>
2026-04-06 13:43:05 +0200

    intro page

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ fetch('content.html')
     });
 
 // =====================
+
+const enterBtn = document.getElementById("enter-btn");
+const intro = document.getElementById("intro-screen");
+const layout = document.getElementById("layout");
+const marquees = document.querySelectorAll(".marquee");
+
+enterBtn.addEventListener("click", () => {
+    intro.style.display = "none";
+    layout.style.display = "grid";
+
+    marquees.forEach(m => {
+        m.style.display = "block";
+    });
+});
